bd8e96c4313acb0a05fe23a78798f836d412fece
mmhzheng <cnplux@gmail.com>
2024-11-13 16:58:49 +0800

    test

---
+++ .history/LOFTest_20241113152101.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from TSB_AD.model_wrapper import run_Unsupervise_AD
+from TSB_AD.evaluation.metrics import get_metrics
+
+# Specify Anomaly Detector to use and data directory
+AD_Name = 'IForest'   # It can be replaced with any anomaly detector availale in TSB-AD
+data_direc = 'Datasets/TSB-AD-U/001_NAB_id_1_Facility_tr_1007_1st_2014.csv'
+
+# Loading Data
+df = pd.read_csv(data_direc).dropna()
+data = df.iloc[:, 0:-1].values.astype(float)
+label = df['Label'].astype(int).to_numpy()
+
+# Applying Anomaly Detector
+output = run_Unsupervise_AD(AD_Name, data)
+
+# Evaluation
+evaluation_result = get_metrics(output, label)
+
+print(evaluation_result)
